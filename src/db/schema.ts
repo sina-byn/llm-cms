5257facde4a2acd4749e183a5c3a4d496e7eb053
sina-byn <sina.sina.bayan@protonmail.com>
2025-10-03 19:01:23 +0330

    chore: conversations and messages schemas impl

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,1 +1,56 @@
 export * from './auth-schema';
+
+import { relations } from 'drizzle-orm';
+
+import {
+  pgTable,
+  uuid,
+  text,
+  pgEnum,
+  varchar,
+  timestamp,
+} from 'drizzle-orm/pg-core';
+
+export const roleEnum = pgEnum('role', ['user', 'assistant', 'system']);
+
+export const conversations = pgTable('conversations', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  title: varchar('title', { length: 255 }).notNull(),
+  scope: varchar('scope', { length: 255 }).notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp('updated_at', { withTimezone: true })
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  deletedAt: timestamp('deleted_at', { withTimezone: true }),
+});
+
+export const messages = pgTable('messages', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  conversationId: uuid('conversation_id')
+    .notNull()
+    .references(() => conversations.id),
+  role: roleEnum('role').notNull(),
+  content: text('content').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp('updated_at', { withTimezone: true })
+    .defaultNow()
+    .$onUpdate(() => new Date())
+    .notNull(),
+  deletedAt: timestamp('deleted_at', { withTimezone: true }),
+});
+
+export const conversationsRelations = relations(conversations, ({ many }) => ({
+  messages: many(messages),
+}));
+
+export const messagesRelations = relations(messages, ({ one }) => ({
+  conversations: one(conversations, {
+    fields: [messages.conversationId],
+    references: [conversations.id],
+  }),
+}));
